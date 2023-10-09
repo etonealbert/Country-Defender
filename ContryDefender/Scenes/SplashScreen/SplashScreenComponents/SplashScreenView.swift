dba7cb6b557e8a224513e3e8f7433bb4d88eb601
Makar <kobliamakar@gmail.com>
2023-10-09 13:35:58 +0300

    Add splash screen

--- ContryDefender/Scenes/SplashScreen/SplashScreenComponents/SplashScreenView.swift
+++ ContryDefender/Scenes/SplashScreen/SplashScreenComponents/SplashScreenView.swift
@@ -5,4 +5,16 @@
 //  Created by Albert Lukmanov on 08/10/2023.
 //
 
-import Foundation
+import UIKit
+
+final class SplashScreenView: UIViewController {
+    
+    @IBOutlet weak var creditsLabel: UILabel!
+    @IBOutlet weak var labelCenterYConstraint: NSLayoutConstraint!
+    
+    var credits: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        creditsLabel.text = credits
+    }
+}
